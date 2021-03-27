48276c03a4867db4292e1eaee378f7640d05c803
lilimin <290059742@qq.com>
2021-03-27 12:05:41 +0800

    增加demo

---
+++ spring-boot-project/learning-spring-boot/src/main/java/org/springframework/boot/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package org.springframework.boot.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@RequestMapping("index")
+    public String index() {
+	    return "hello world";
+    }
+}
---
+++ spring-boot-project/learning-spring-boot/src/test/java/org/springframework/boot/DemoApplicationTest.java
@@ -0,0 +1,7 @@
+package org.springframework.boot;
+
+/**
+ * Unit test for simple DemoApplication.
+ */
+public class DemoApplicationTest {
+}
